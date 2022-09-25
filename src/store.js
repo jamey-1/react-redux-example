37d94a8d75b5836cd9dc50cd26a534f6d1cb983c
YEMING LIU <liuyeming@zhouyang.net>
2022-09-24 23:55:40 -0600

    using configureStore

--- src/store.js
+++ src/store.js
@@ -1,15 +1,24 @@
-import { createStore, combineReducers } from "redux";
+// import { createStore, combineReducers } from "redux";
+// import { todos } from "./todos/reducers";
+
+// const reducers = {
+//     todos,
+// };
+
+// const rootReducer = combineReducers(reducers);
+
+// export const configureStore = () => 
+//     createStore(
+//      rootReducer,
+//      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//      window.__REDUX_DEVTOOLS_EXTENSION__(),
+//     );
+
+import { configureStore } from "@reduxjs/toolkit";
 import { todos } from "./todos/reducers";
 
-const reducers = {
-    todos,
-};
-
-const rootReducer = combineReducers(reducers);
-
-export const configureStore = () => 
-    createStore(
-     rootReducer,
-     window.__REDUX_DEVTOOLS_EXTENSION__ &&
-     window.__REDUX_DEVTOOLS_EXTENSION__(),
-    );
+export default configureStore({
+    reducer: {
+        todos,
+    }
+});
